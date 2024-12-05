96f43c8da5c25d2b49a7463cce9f2b45f5046b2a
Riley <rileyeverett16@outlook.com>
2024-12-05 00:38:31 +0000

    mk1 completed as well as mk2 config file

--- exported_vars.py
+++ exported_vars.py
@@ -1,22 +1,31 @@
 import os
 import sys
 import subprocess
+hardwareinfo =('''\033[33m
+ _    _               _                        
+| |  | |             | |                       
+| |__| | __ _ _ __ __| |_      ____ _ _ __ ___ 
+|  __  |/ _` | '__/ _` \ \ /\ / / _` | '__/ _ \\
+| |  | | (_| | | | (_| |\ V  V / (_| | | |  __/
+|_|  |_|\__,_|_|  \__,_| \_/\_/ \__,_|_|  \___|                                               
+ _        __      
+(_)      / _|     
+ _ _ __ | |_ ___  
+| | '_ \|  _/ _ \ 
+| | | | | || (_) |
+|_|_| |_|_| \___/ 
+                                               
+\033[0m''')
+              
+print(hardwareinfo)
 
-import subprocess
-
-# Path to your Ansible playbook
-playbook_path = "playbook.yml"
-
-# Run the playbook using subprocess
 try:
     result = subprocess.run(
-        ['ansible-playbook', playbook_path],
+        ['ansible-playbook', 'playbook.yml'],
         capture_output=True,  
         text=True,            
         check=True            
     )
-    print("Playbook Output:")
-    print(result.stdout)
 except subprocess.CalledProcessError as e:
     print(f"Error running playbook: {e}")
     print(f"Standard Error: {e.stderr}")
@@ -26,7 +35,9 @@ info=[]
 for words in file:
     if words.strip():
         info.append(words.strip())
-appleart = ('''
+
+if info[0] =='MacOSX':
+    appleart = ('''
 \033[34m                   'c.
                  ,xNMM.          \033[33m--------------------------------\033[34m
                .OMMMMo           \033[33mOS: \033[0m{0}\033[34m
@@ -45,5 +56,25 @@ appleart = ('''
      ;KMMMMMMMWXXWMMMMMMMk.      
        .cooc,.    .,coo:. \033[0m
 ''').format(*info)
-
-print(appleart)
+    print(appleart)
+if info[0] == 'Linux':
+    # maybe not linux will need to test
+    linuxart = (r'''
+         _nnnn_
+        dGGGGMMb            -------------------------------- 
+       @p~qp~~qMb           OS: {0}
+       M|@||@) M|           Architecture: {1}
+       @,----.JM|           Processor: {2}
+      JS^\__/  qKL          Cores: {3}
+     dZP        qKRb        Vcores: {4}
+    dZP          qKKb       Memory: {6} / {5}
+   fZP            SMMb      Uptime: {7}
+   HZM            MMMM      --------------------------------
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--' hjm
+                ''').format(*info)
+    print(linuxart)
